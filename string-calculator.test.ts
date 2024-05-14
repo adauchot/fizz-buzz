921c755238bca39b59656cceec9cd436953b3d02
aurelien <aurelien.dauchot@soprism.com>
2024-05-14 09:23:12 +0200

    Red phase test 1

---
+++ string-calculator.test.ts
@@ -0,0 +1,6 @@
+describe('StringCalculator', () => {
+    it('should return the sum of 2 numbers in the string', () => {
+        expect(StringCalculator.add("1,2")).toBe(3);
+        expect(StringCalculator.add("3,4")).toBe(7);
+    });
+});
