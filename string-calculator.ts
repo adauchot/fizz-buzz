dfe0d0129b420e399d2b549f69576afab749d3d8
aurelien <aurelien.dauchot@soprism.com>
2024-05-14 09:27:42 +0200

    Blue phase test 1

--- string-calculator.ts
+++ string-calculator.ts
@@ -1,8 +1,6 @@
 export class StringCalculator {
     static add(numbers: string): number {
-        const splitNumbers = numbers.split(",");
-        const number1 = parseInt(splitNumbers[0]);
-        const number2 = parseInt(splitNumbers[1]);
-        return number1 + number2;
+        return numbers.split(",")
+            .reduce((sum, num) => sum + parseInt(num), 0);
     }
 }
