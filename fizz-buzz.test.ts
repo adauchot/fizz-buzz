6202b6c2dba8a11299d40f182f51689cf76ec03e
aurelien <aurelien.dauchot@soprism.com>
2024-05-14 09:18:10 +0200

    Updating the tests

--- fizz-buzz.test.ts
+++ fizz-buzz.test.ts
@@ -7,17 +7,17 @@ describe('FizzBuzz', () => {
 
     it('should return Fizz', () => {
         expect(FizzBuzz.fizzBuzz(3)).toBe("Fizz");
+        expect(FizzBuzz.fizzBuzz(18)).toBe("Fizz");
     });
 
     it('should return Buzz', () => {
         expect(FizzBuzz.fizzBuzz(5)).toBe("Buzz");
+        expect(FizzBuzz.fizzBuzz(20)).toBe("Buzz");
     });
 
-    it('should return 4', () => {
+    it('should return the number', () => {
         expect(FizzBuzz.fizzBuzz(4)).toBe(4);
-    });
-
-    it('should return 31', () => {
+        expect(FizzBuzz.fizzBuzz(7)).toBe(7);
         expect(FizzBuzz.fizzBuzz(31)).toBe(31);
     });
 });
