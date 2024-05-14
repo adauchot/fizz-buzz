1300d76736457f9de68380a3ff624a4430a0f7c9
aurelien <aurelien.dauchot@soprism.com>
2024-05-14 09:29:16 +0200

    Red phase test 2

--- string-calculator.test.ts
+++ string-calculator.test.ts
@@ -5,4 +5,13 @@ describe('StringCalculator', () => {
         expect(StringCalculator.add("1,2")).toBe(3);
         expect(StringCalculator.add("3,4")).toBe(7);
     });
+
+    it('should return the number in the string', () => {
+        expect(StringCalculator.add("5")).toBe(5);
+        expect(StringCalculator.add("6")).toBe(6);
+    });
+
+    it('should return 0 for an empty string', () => {
+        expect(StringCalculator.add("")).toBe(0);
+    });
 });
