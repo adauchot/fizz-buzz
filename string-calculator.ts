e708bb09b46dac0af25901f9413bcffd21c64219
aurelien <aurelien.dauchot@soprism.com>
2024-05-14 09:56:54 +0200

    Green/Blue phase test 4

--- string-calculator.ts
+++ string-calculator.ts
@@ -2,7 +2,7 @@ export class StringCalculator {
     static add(numbers: string): number {
         if (numbers === "") return 0;
 
-        return numbers.split(",")
+        return numbers.split(/[,\n]/)
             .reduce((sum, num) => sum + parseInt(num), 0);
     }
 }
