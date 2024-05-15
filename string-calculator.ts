923b25f57c30bb7aa1fccf33c67a84328ef3f439
aurelien <aurelien.dauchot@soprism.com>
2024-05-15 09:54:39 +0200

    Green/Blue phase test 6

--- string-calculator.ts
+++ string-calculator.ts
@@ -2,6 +2,12 @@ export class StringCalculator {
     static add(numbers: string): number {
         if (numbers === "") return 0;
 
+        const negativeNumbersRegex = /-\d+/g;
+        const negativeNumbers = numbers.match(negativeNumbersRegex);
+        if (negativeNumbers && negativeNumbers.length > 0) {
+            throw new Error(`Negatives are not allowed: ${negativeNumbers.join(",")}`);
+        }
+
         let delimiter = /[,\n]/;
 
         const delimiterRegex = /\/\/(.)\n/;
