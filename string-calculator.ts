c6c9bbb44c09a25ba055e1f7442aaae92ae47e6b
aurelien <aurelien.dauchot@soprism.com>
2024-05-15 09:22:04 +0200

    Green phase test 5

--- string-calculator.ts
+++ string-calculator.ts
@@ -2,6 +2,13 @@ export class StringCalculator {
     static add(numbers: string): number {
         if (numbers === "") return 0;
 
+        if (numbers.startsWith("//")) {
+            const delimiter = numbers[2];
+            numbers = numbers.slice(4);
+            return numbers.split(delimiter)
+                .reduce((sum, num) => sum + parseInt(num), 0);
+        }
+
         return numbers.split(/[,\n]/)
             .reduce((sum, num) => sum + parseInt(num), 0);
     }
